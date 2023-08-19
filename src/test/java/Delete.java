3ce9e2b6dde72219342a9acfc7fff48b8aaf31eb
SatishVenka <satishvenket26@gmail.com>
2023-08-19 20:48:24 +0530

    commitmsg

---
+++ src/test/java/Delete.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class Delete {
+@Test
+	public void delete() {
+	System.out.println("------delete the folder---");
+		// TODO Auto-generated constructor stub
+	}
+
+}
